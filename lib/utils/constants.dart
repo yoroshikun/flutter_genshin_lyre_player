972caef80cad1563c40492586bdd54f5dc81e3c5
Drew Hutton Yoroshi <drew.business2308@gmail.com>
2021-04-03 15:17:43 +1030

    feat: custom font

---
+++ lib/utils/constants.dart
@@ -0,0 +1,98 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+const colorBlack = Color.fromRGBO(48, 47, 48, 1.0);
+const colorGray = Color.fromRGBO(141, 141, 141, 1.0);
+
+const colorWhite = Colors.white;
+const colorMain = Color.fromRGBO(165, 55, 253, 1.0);
+const colorSecondary = Color.fromRGBO(159, 90, 253, 1.0);
+
+const Typography textThemeDefault = Typography(
+  header: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorBlack,
+      fontSize: 46,
+      height: 56,
+      fontWeight: FontWeight.w300),
+  subheader: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorGray,
+      fontSize: 34,
+      height: 40,
+      fontWeight: FontWeight.w300),
+  title: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorBlack,
+      fontSize: 24,
+      height: 28,
+      fontWeight: FontWeight.w400),
+  subtitle: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorBlack,
+      fontSize: 20,
+      height: 24,
+      fontWeight: FontWeight.w400),
+  base: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorBlack,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.w500),
+  body: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorBlack,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.w400),
+  caption: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorBlack,
+      fontSize: 12,
+      height: 14,
+      fontWeight: FontWeight.w400),
+);
+
+const Typography textThemeDark = Typography(
+  header: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorWhite,
+      fontSize: 46,
+      height: 56,
+      fontWeight: FontWeight.w300),
+  subheader: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorGray,
+      fontSize: 34,
+      height: 40,
+      fontWeight: FontWeight.w300),
+  title: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorWhite,
+      fontSize: 24,
+      height: 28,
+      fontWeight: FontWeight.w400),
+  subtitle: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorWhite,
+      fontSize: 20,
+      height: 24,
+      fontWeight: FontWeight.w400),
+  base: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorWhite,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.w500),
+  body: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorWhite,
+      fontSize: 14,
+      height: 20,
+      fontWeight: FontWeight.w400),
+  caption: TextStyle(
+      fontFamily: 'Manrope',
+      color: colorWhite,
+      fontSize: 12,
+      height: 14,
+      fontWeight: FontWeight.w400),
+);
